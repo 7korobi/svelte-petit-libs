b3f45785bccdd5eec6af8f336c0bb3103beef8c5
aTomohiroIto <tomohiro.ito@access-company.com>
2021-01-01 23:03:22 +0900

    download.

--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -71,3 +71,32 @@ export function readyWatching(): Promise<void> {
     }
   })
 }
+
+export function readyDownload(
+  el: HTMLImageElement | HTMLIFrameElement,
+  url: string,
+  timeout = 20000
+): Promise<Event> {
+  return new Promise((ok, ng) => {
+    const timer = setTimeout(fail, timeout)
+    el.addEventListener("error", fail)
+    el.addEventListener("load", success)
+    el.src = url
+
+    function bye() {
+      el.removeEventListener("error", fail)
+      el.removeEventListener("load", success)
+    }
+
+    function fail(e: Event = new Event(`timeout ${timeout / 1000}sec`)) {
+      el.src = ""
+      bye()
+      ng(e)
+    }
+
+    function success(e: Event) {
+      bye()
+      ok(e)
+    }
+  })
+}
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -9,3 +9,7 @@ type height = number
 export type SIZE = [width, height]
 export type POINT = [left, top]
 export type OFFSET = [top, right, bottom, left]
+
+export type DIC<T> = {
+  [key: string]: T
+}
