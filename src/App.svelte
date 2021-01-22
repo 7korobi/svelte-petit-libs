1feaf6d08f8a62991b0bc4ab40ebf6bfb442119e
7korobi <7korobi@gmail.com>
2021-01-23 03:54:36 +0900

    人狼議事Layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,47 @@
 <script lang="ts">
   import { Calendar } from "fancy-date/lib/sample"
+  import { isOnline } from "./lib/browser"
+
   import Giji from "./layout/Giji.svelte"
   import Chat from "./chat/Chat.svelte"
+
+  import Scroll from "./block/Scroll.svelte"
   import KeyBoard from "./head/KeyBoard.svelte"
   import FullScreen from "./head/FullScreen.svelte"
   import Poll from "./data/Poll.svelte"
   import RoosterArea from "./form/RoosterArea.svelte"
-  import { isOnline } from "./lib/browser"
+  import Diagram from "./svg/Diagram.svelte"
+  import Banner from "./chat/Banner.svelte"
 
   export let name: string
 
   let fs
   let c = 0
   let f = 32
-  let news = [""]
+  let ymd = ""
+  let texts = []
+  let rubys = []
   let count = 0
   let rest = "th"
   let color = "black"
   let data = []
+
+  let icons = [
+    { v: "c01", roll: 0, x: -200, y: -100, label: "花売り メアリー" },
+    { v: "c02", roll: 0, x: 200, y: -100, label: "村長をそんちょうしよう！" },
+    { v: "c03", roll: 90, x: -200, y: 100, label: "1st" },
+    { v: "c04", roll: 180, x: 0, y: 180, label: "" },
+    { v: "c05", roll: 270, x: 200, y: 100, label: "3rd" },
+  ]
+  let lines = [
+    { v: "c01", w: "c02", vpos: 0, wpos: 0, line: " = ", label: "ふれんず" },
+    { v: "c01", w: "c02", vpos: 180, wpos: 0, line: " ->", label: "フレンズ" },
+    { v: "c01", w: "c02", vpos: 0, wpos: 180, line: "<- ", label: "ふれんず" },
+    { v: "c01", w: "c02", vpos: 180, wpos: 180, line: "o.x", label: "" },
+  ]
+  let clusters = [{ vs: ["c01"], label: "グループ" }]
+
+  // $: console.log(group, core, mode, rect)
 
   $: rest = [undefined, "st", "nd", "rd"][count] ?? "th"
 
@@ -28,12 +52,15 @@
   }
 
   setTimeout(() => {
-    news = Calendar.平気法
+    const data = Calendar.平気法
       .format(
         new Date().getTime(),
-        "Gy年Mod日 Homo Hrmr ao ar Ao Ar Eo Er Fo Fr No Nr Zo Zr"
+        "Gy年Mod日Mr|Ho mo ao Ao Eo Fo No Zo|Hr mr ar Ar Er Fr Nr Zr"
       )
-      .split(" ")
+      .split("|")
+    ymd = data[0]
+    texts = data[1].split(" ")
+    rubys = data[2].split(" ")
   }, 2000)
 
   async function reqPlan() {
@@ -62,63 +89,116 @@
     f = +str
     c = +((5 / 9) * (f - 32)).toFixed(1)
   }
+
+  function intersection(e) {
+    console.log(e.detail)
+  }
 </script>
 
 <Giji max={2.0}>
-  <FullScreen bind:toggle={fs}>
-    <Chat show="report" handle="VSSAY">
-      <h1 class="c">Hello {name}! {$isOnline ? 'online' : 'offline'}</h1>
-      <p class="c">
-        {@html news.join('<br />')}
-      </p>
-      <p>
-        Visit the
-        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-        to learn how to build Svelte apps.
-      </p>
-    </Chat>
-    <Chat show="talk" handle="PSAY" face_id="c03">
-      <RoosterArea />
-      <button on:click={fs}>full screen</button>
-    </Chat>
-  </FullScreen>
+  <Scroll name=".1" on:change={intersection}>
+    <FullScreen bind:toggle={fs}>
+      <Banner>
+        <h3>.1.1</h3>
+      </Banner>
+      <Chat show="report" handle="VSSAY">
+        <h1 class="c">Hello {name}! {$isOnline ? 'online' : 'offline'}</h1>
+        <p>
+          {ymd}
+          {#each texts as text, idx (text)}
+            <ruby>{text}
+              <rt>{rubys[idx]}</rt></ruby>
+          {/each}
+        </p>
+        <p>
+          Visit the
+          <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+          to learn how to build Svelte apps.
+        </p>
+      </Chat>
+      <Banner>
+        <h3>.1.2</h3>
+      </Banner>
+      <Chat show="talk" handle="PSAY" id="rooster" face_id="c03">
+        <RoosterArea />
+        <button on:click={fs}>full screen</button>
+      </Chat>
+    </FullScreen>
+  </Scroll>
 
-  <Chat show="post" handle="TSAY">
-    <button on:click={handleClick}>Clicked
-      <span style={`color: ${color}`}>{count}{rest}</span></button>
-  
-    <!-- https://github.com/eugenkiss/7guis/wiki#temperature-converter -->
-    <input value={c} on:input={(e) => byC(e.currentTarget.value)} type="number" />
-    °c =
-    <input value={f} on:input={(e) => byF(e.currentTarget.value)} type="number" />
-    °f
-    <style>
-      input {
-        width: 5em;
-      }
-    </style>
-  </Chat>
+  <Scroll name=".2" on:change={intersection}>
+    <Banner>
+      <h3>.2</h3>
+    </Banner>
+    <Scroll name=".2.1" on:change={intersection}>
+      <Chat show="post" handle="TSAY">
+        <button on:click={handleClick}>Clicked
+          <span style={`color: ${color}`}>{count}{rest}</span></button>
+
+        <!-- https://github.com/eugenkiss/7guis/wiki#temperature-converter -->
+        <input
+          value={c}
+          on:input={(e) => byC(e.currentTarget.value)}
+          type="number" />
+        °c =
+        <input
+          value={f}
+          on:input={(e) => byF(e.currentTarget.value)}
+          type="number" />
+        °f
+        <style>
+          input {
+            width: 5em;
+          }
+        </style>
+      </Chat>
+    </Scroll>
+    <Scroll name=".2.2" on:change={intersection}>
+      <Chat show="report" handle="SSAY">
+        <Diagram {icons} {lines} {clusters} />
+      </Chat>
+    </Scroll>
+  </Scroll>
+
+  <Scroll name=".3" on:change={intersection}>
+    <Banner>
+      <h3>.3</h3>
+    </Banner>
+    <Scroll name=".3.1" on:change={intersection}>
+      <Chat show="post" handle="TSAY">
+        <h1 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h1>
+        <hr />
+        <h2 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h2>
+        <hr />
+        <h3 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h3>
+        <hr class="stripe" />
+        <hr class="blank" />
+        <hr class="stripe" />
+        <h4 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h4>
+        <hr class="footnote" />
+        <h5 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h5>
+        <hr class="footnote" />
+        <h6 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h6>
+      </Chat>
+    </Scroll>
+    <Scroll name=".3.2" on:change={intersection}>
+      <Chat show="talk" handle="WSAY" face_id="t10" head="abc">
+        <h1 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h1>
+        <hr />
+        <h2 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h2>
+        <hr />
+        <h3 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h3>
+        <hr class="stripe" />
+        <hr class="blank" />
+        <hr class="stripe" />
+        <h4 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h4>
+        <hr class="footnote" />
+        <h5 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h5>
+        <hr class="footnote" />
+        <h6 class="c">こんにちは、Svelte世界！　Hello SVELTE world!</h6>
+      </Chat>
+    </Scroll>
+  </Scroll>
 </Giji>
 <KeyBoard on:key={keyDown} on:combo={keyCombo} />
 <Poll timer="6h" api={reqPlan} />
-
-<style lang="scss">
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-    h1 {
-      color: #ff3e00;
-      text-transform: uppercase;
-      font-size: 4em;
-      font-weight: 100;
-    }
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
--- src/layout/Giji.svelte
+++ src/layout/Giji.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-import ThemeBtns from "../inline/ThemeBtns.svelte";
-import HeadViewport from "../head/HeadViewport.svelte"
-import SafeArea from "../head/SafeArea.svelte"
+  import ThemeBtns from "../inline/ThemeBtns.svelte"
+  import HeadViewport from "../head/HeadViewport.svelte"
+  import SafeArea from "../head/SafeArea.svelte"
+  import Report from "../chat/Report.svelte"
 
-export let max = 1.0
-export let min = 1.0
+  export let max = 1.0
+  export let min = 1.0
 
-function resize() {}
+  function resize() {}
 
-function scroll() {}
-
+  function scroll() {}
 </script>
 
 <div class="page-active-bg">
@@ -18,11 +18,61 @@ function scroll() {}
       <ThemeBtns />
       <div class="contentframe">
         <div class="inframe">
-          <slot>
-            ここにコンテンツを書きます。
-          </slot>
+          <slot>ここにコンテンツを書きます。</slot>
         </div>
       </div>
+      <div class="toastframe">
+        <div class="inframe">
+          <Report handle="ADMIN">
+            <div class="text fine mono">
+              <p class="r">
+                <span class="limit1-ok">o</span>
+                <span class="limit1-ng">-</span>
+                <span class="limit2-ok">o</span>
+                <span class="limit2-ng">-</span>
+                <span class="limit3-ok">o</span>
+                <span class="limit3-ng">-</span>
+              </p>
+              <p
+                data-tooltip="不十分な画面幅にあわせ、折返し表示をしています"
+                class="limit1-ng tooltip-left">
+                折返中
+              </p>
+            </div>
+          </Report>
+          <div class="icons form">
+            <a
+              class="btn item-half tooltip-left"
+              data-tooltip="詳細情報を拡げる操作の ON / OFF">
+              <i class="mdi mdi-arrow-expand-right" />
+            </a>
+            <a
+              class="btn item-half tooltip-left active"
+              data-tooltip="ページ一覧を一列にする / 折り返す">
+              <i class="mdi mdi-gesture-swipe" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="sideframe">
+        <div class="inframe">
+          <div class="icons form">
+            <a
+              class="btn item-half tooltip-left"
+              data-tooltip="詳細情報を拡げる操作の ON / OFF">
+              <i class="mdi mdi-arrow-expand-right" />
+            </a>
+            <a
+              class="btn item-half tooltip-left active"
+              data-tooltip="ページ一覧を一列にする / 折り返す">
+              <i class="mdi mdi-gesture-swipe" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="summaryframe impose" />
+      <div class="center-left" />
+      <div class="center-right" />
     </div>
   </div>
 </div>
